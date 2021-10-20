b057eaf0ca6ceca8675e02fdad4411272b792fa3
ranaazmy <ranaazmy95@gmail.com>
2021-10-20 11:47:34 +0200

    Listening to Events & Working with Event Handlers

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -2,12 +2,16 @@ import ExpenseDate from "../Expenses/ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 function ExpenseItem(props) {
+    function clickHandler(){
+        console.log('Clicked!!!!');
+    }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+          <button onClick={clickHandler}>Change Title</button>
       </div>
     </Card>
   );
