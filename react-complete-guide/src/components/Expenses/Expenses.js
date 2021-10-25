0e572a28b538d2e52590210c4801cfacb987c57c
ranaazmy <ranaazmy95@gmail.com>
2021-10-25 09:50:30 +0200

    Using keys

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -7,16 +7,19 @@ import ExpensesFilter from "./ExpenseFilter";
 
 function Expenses(props) {
     function optionSave(selectedYear) {
-        console.log('In Expenses Js');
         console.log(selectedYear);
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+    // return expense.date.getFullYear().toString()
+    });
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter onOptionSave={optionSave}/>
                 {props.items.map((expense) => (
                     <ExpenseItem
+                        key={expense.id}
                         title={expense.title}
                         amount={expense.amount}
                         date={expense.date}
