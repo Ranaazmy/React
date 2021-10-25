f03516a285ad36a9b2538de6962845b7736806a2
ranaazmy <ranaazmy95@gmail.com>
2021-10-25 11:13:51 +0200

    Update Expenses.js

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -16,20 +16,24 @@ function Expenses(props) {
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === enteredOption;
     });
+
+    let expensesContent = <p>No Expenses Found.</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ))
+    }
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={enteredOption}
                                 onOptionSave={optionSave}/>
-                {filteredExpenses.length == 0 ? <p>No Expenses Found!</p> : filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
-                
+                {expensesContent}
             </Card>
         </div>
     );
