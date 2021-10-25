40bc9d2137f69ae3541f0e1d6143dfaed3f3522d
ranaazmy <ranaazmy95@gmail.com>
2021-10-25 12:41:44 +0200

    Adding a Chart

---
+++ react-complete-guide/src/components/Chart/Chart.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import './Chart.css';
+import ChartBar from "./ChartBar";
+
+
+function Chart(props) {
+    return (
+        <div className="chart">
+            {props.dataPoints.map((dataPoint) => (
+                <ChartBar
+                    value = {dataPoint.value}
+                    maxValue = {null}
+                    label = {dataPoint.label}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default Chart;
---
+++ react-complete-guide/src/components/Chart/ChartBar.js
@@ -0,0 +1,9 @@
+import React, {useState} from "react";
+import './ChartBar.css';
+
+
+function ChartBar(props) {
+
+}
+
+export default ChartBar;
